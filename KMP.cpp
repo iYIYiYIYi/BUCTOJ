99b696d98b68bfb3c3adbe6541e35d253bf85469
iYIYiYIYi <yihuang728@qq.com>
2019-11-06 11:27:39 +0800

    Update KMP.cpp
    this is a version of KMP

--- KMP.cpp
+++ KMP.cpp
@@ -1,10 +1,27 @@
-#include<iosteam>
+
+#include<iostream>
 using namespace std;
+#include <string.h>
+#define Maxsize 100
+typedef char Mystring[Maxsize];
 
-
-
-
+int index(Mystring M, Mystring P){
+    int pi = 0,mi = 0,plen = strlen(P),mlen = strlen(M);
+    while(pi<plen&&mi<mlen){
+        if(P[pi]==M[mi]){
+			mi++;
+            pi++;
+        }else {
+        	mi = mi - pi;
+            pi = 0;
+        }
+    }
+    if(pi == plen)
+    	return mi-pi;
+    else
+    	return -1;
+}
 int main(){
 
-	return 0;
+
 }
